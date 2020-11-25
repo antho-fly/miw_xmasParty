77acd34691d0f4228686b46bad83ada917b69996
anthobrc <brochieranthony.pro@gmail.com>
2020-11-25 08:48:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+if (isset($_POST['email'])) {
+
+    header('Location: send_email.php?email=' . $_POST['email']);
+    exit;
+}
+
+$form = "formulaire";
+$send = 'hide';
+
+
+if (isset($_GET['delivery']) and $_GET['delivery'] == "sent") {
+    $form = 'hide';
+    $send = 'show';
+}
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MIW - Day 3</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="main">
+            <form action="#" method="post" class="<?php echo $form ?>">
+                <input id="email" class="inpForm" type="email" name="email" placeholder="Ton email de star..." required aria-label="Input Email"/>
+                <button class="bg-mauve inpSub" type="submit">
+                    Inscris-toi !
+                    <img class="vip" src="images/icones/vip.png">
+                </button>
+            </form>
+            <div class="cadreEmailSend <?php echo $send?>">
+                <img src="images/icones/send.png">
+            </div>
+        </div>
+    </body>
+</html>
